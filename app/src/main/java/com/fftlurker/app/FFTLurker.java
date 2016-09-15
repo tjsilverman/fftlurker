784a5830160377eeeeaae45df8635c6031f3e6eb
tsilverman <tsilverman@live.com.au>
2016-09-15 15:47:44 +1000

    Initial commit.

---
+++ app/src/main/java/com/fftlurker/app/FFTLurker.java
@@ -0,0 +1,15 @@
+package com.fftlurker.app;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class FFTLurker extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fftlurker);
+    }
+
+}
